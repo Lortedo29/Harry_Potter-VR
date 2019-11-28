d333f989f95e26c12850ad78f57eebac34761dcf
Theo <theo.farnole@gmail.com>
2019-11-28 16:00:29 +0100

    Avoid OOB exception

--- Assets/Scripts/Game/Hub/Atelier HUB/AtelierAnneau/AnneauAtelierTravel.cs
+++ Assets/Scripts/Game/Hub/Atelier HUB/AtelierAnneau/AnneauAtelierTravel.cs
@@ -9,7 +9,7 @@ public class AnneauAtelierTravel : Singleton<AnneauAtelierTravel>
     [SerializeField] private GameObject[] _wayPoints;
     [SerializeField] private GameObject _plume;
 
-    public int _numberAnneauReach = 0;    
+    public int _numberAnneauReach = 0;
     public float _timerBetwinTwoCircle = 7f;
 
     void Start()
@@ -23,7 +23,7 @@ public class AnneauAtelierTravel : Singleton<AnneauAtelierTravel>
         _timerBetwinTwoCircle = 7; // reset timer
 
         // check for victory
-        if (_numberAnneauReach == _wayPoints.Length + 1)
+        if (_numberAnneauReach >= _wayPoints.Length + 1)
         {
             Victoire();
 
@@ -34,6 +34,7 @@ public class AnneauAtelierTravel : Singleton<AnneauAtelierTravel>
         int index = _numberAnneauReach - 1;
         _wayPoints[index].SetActive(true);
 
+        // deactivate the last waypoint
         if (index - 1 >= 0)
         {
             _wayPoints[index - 1].SetActive(false);
@@ -42,12 +43,15 @@ public class AnneauAtelierTravel : Singleton<AnneauAtelierTravel>
 
     void Defaite()
     {
-        int index = _numberAnneauReach - 1;
-        _wayPoints[index].SetActive(false);
-
         _numberAnneauReach = 0;
         _timerBetwinTwoCircle = 7f;
         ManagerAudio.Instance.PlaySound(ManagerAudio.Sound.LooseSound);
+
+        // deactive every way point
+        foreach (var waypoint in _wayPoints)
+        {
+            waypoint.SetActive(false);
+        }
     }
 
     void Victoire()
@@ -73,152 +77,7 @@ public class AnneauAtelierTravel : Singleton<AnneauAtelierTravel>
             {
                 Defaite();
             }
-        }
-
-        return;
-
-        //switch (_numberAnneauReach)
-        //{
-        //    case 0:
-        //        _timerBetwinTwoCircle = 7f;
-        //        break;
-        //    case 1:
-        //        _timerBetwinTwoCircle = 7f;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint1.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint1.SetActive(true);
-        //        break;
-        //    case 2:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint2.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint1.SetActive(false);
-        //        _wayPoint2.SetActive(true);
-        //        break;
-        //    case 3:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint3.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint2.SetActive(false);
-        //        _wayPoint3.SetActive(true);
-        //        break;
-        //    case 4:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint4.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint3.SetActive(false);
-        //        _wayPoint4.SetActive(true);
-        //        break;
-        //    case 5:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint5.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint4.SetActive(false);
-        //        _wayPoint5.SetActive(true);
-        //        break;
-        //    case 6:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint6.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint5.SetActive(false);
-        //        _wayPoint6.SetActive(true);
-        //        break;
-        //    case 7:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint7.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint6.SetActive(false);
-        //        _wayPoint7.SetActive(true);
-        //        break;
-        //    case 8:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint8.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint7.SetActive(false);
-        //        _wayPoint8.SetActive(true);
-        //        break;
-        //    case 9:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint9.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint8.SetActive(false);
-        //        _wayPoint9.SetActive(true);
-        //        break;
-        //    case 10:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _wayPoint10.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint9.SetActive(false);
-        //        _wayPoint10.SetActive(true);
-        //        break;
-        //    case 11:
-        //        _timerBetwinTwoCircle = 7f;
-        //        _timerBetwinTwoCircle -= Time.deltaTime;
-        //        if (_timerBetwinTwoCircle <= 0)
-        //        {
-        //            _waypoint11.SetActive(false);
-        //            _numberAnneauReach = 0;
-        //            _timerBetwinTwoCircle = 7f;
-        //        }
-        //        _wayPoint10.SetActive(false);
-        //        _waypoint11.SetActive(true);
-
-        //        break;
-        //    case 12:
-
-        //        _timerBetwinTwoCircle = 7f;
-        //        _numberAnneauReach = 0;
-        //        break;
-
+        }              
     }
 }
 
